1bf87ee006ff15030807f8b59bc5d610b4edcae4
Mateusz Kyc <mateusz.kyc@gmail.com>
2018-02-18 16:40:48 +0100

    podstawka

--- src/main/java/pl/alfons/app/controllers/MainPageController.java
+++ src/main/java/pl/alfons/app/controllers/MainPageController.java
@@ -1,16 +1,25 @@
 package pl.alfons.app.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.alfons.app.services.DummyService;
 
 @Controller
 public class MainPageController {
 
+    @Autowired
+    DummyService dummyService;
+
     @GetMapping(value = "/")
     public String mainPage(Model model) {
-        model.addAttribute("name", "to jest nazwa");
+        model.addAttribute("name", dummyService.getName());
         return "mainPage";
+    }
+    @GetMapping(value="/tp")
+    public String creatingProject (){
+        return "creatingProject";
     }
 }
