ce32ffd6c8b89ebeb43c580865e81e8e2189223c
Mateusz Kyc <mateusz.kyc@gmail.com>
2018-04-09 22:27:14 +0200

    dodanie Lomboka w Inellij:  - Preferences -> Plugins -> wyszukujemy Lombok -> instalujemy -> restartujemy intellij  - Preferences -> Other settings -> Lombok plugin -> Enable lombok for this project Od tego momentu ma się budowac

--- src/main/java/pl/alfons/app/controllers/MainPageController.java
+++ src/main/java/pl/alfons/app/controllers/MainPageController.java
@@ -1,12 +1,14 @@
 package pl.alfons.app.controllers;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.alfons.app.services.SmartService;
 
+@Slf4j
 @Controller
 public class MainPageController {
 
@@ -17,6 +19,7 @@ public class MainPageController {
     @GetMapping(value = "/")
     public String mainPage(Model model) {
         model.addAttribute("name", smartService.getName());
+        log.debug("lombok debug: " + smartService.getName());
         return "mainPage";
     }
 
