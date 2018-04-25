012c1cf8165cee41234afbf4608a74331547152f
Jakub <jakub92kyc@gmail.com>
2018-04-25 21:30:04 +0200

    tworzenie i zapisywanie taska w repo przekierowanie na widok projektu po submicie

--- src/main/java/pl/alfons/app/controllers/TaskViewController.java
+++ src/main/java/pl/alfons/app/controllers/TaskViewController.java
@@ -8,26 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 import pl.alfons.app.forms.TaskForm;
-import pl.alfons.app.services.ProjectService;
+import pl.alfons.app.services.TaskService;
 
 @Slf4j
 @Controller
 public class TaskViewController {
 
     @Autowired
-    ProjectService projectService;
+    TaskService taskService;
 
-    @GetMapping(value = "wp/{id}/tt")
+    @GetMapping(value = "/wp/{id}/tt")
     public String creatingTask(Model model, @PathVariable String id) {
         model.addAttribute("task", new TaskForm());
         return "creatingTask";
     }
 
-    @PostMapping ("wp/{id}/tt")
-    public String taskSubmit(@ModelAttribute TaskForm task, @PathVariable String id) {
-        System.out.println(" checkpoint 1. id: "+id);
+    @PostMapping ("/wp/{id}/tt")
+    public RedirectView taskSubmit(@ModelAttribute TaskForm task, @PathVariable String id) {
         log.debug("lombok debug: checkpoint 1.  id: " +id);
-        return "dsqadnk";
+        taskService.saveTask(task, id);
+        return new RedirectView("/wp/"+id);
     }
 }
--- src/main/java/pl/alfons/app/services/TaskService.java
+++ src/main/java/pl/alfons/app/services/TaskService.java
@@ -2,6 +2,10 @@ package pl.alfons.app.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.alfons.app.entities.Project;
+import pl.alfons.app.entities.Task;
+import pl.alfons.app.forms.TaskForm;
+import pl.alfons.app.repositories.ProjectRepository;
 import pl.alfons.app.repositories.TaskRepository;
 
 @Service
@@ -10,7 +14,18 @@ public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
-//    public Long saveTask (TaskForm taskForm){
-//
-//    }
+    @Autowired
+    private ProjectRepository projectRepository;
+
+
+    public void saveTask (TaskForm taskForm, String id){
+    Long longId = Long.valueOf(id);
+    Task newTask = new Task(taskForm.getName());
+    Project newProject = projectRepository.findOne(longId);
+    newProject.getTasks().add(newTask);
+    // czy task jest podpięty do projectu w bazie danych czy tylko tu lokalnie?
+    // niby redirectview po submicie wyświetla uaktualnioną listę tasków czyli chyba tak
+    newTask.setProject(newProject);
+    taskRepository.save(newTask);
+    }
 }
